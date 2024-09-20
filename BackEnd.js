9239a2fc95df9c6ef4447b5fcee8a6fa66831286
Nasrolla <Nasrolla.hassani@gmail.com>
2024-09-20 21:04:00 +0200

    final touch v0!

--- BackEnd.js
+++ BackEnd.js
@@ -1,7 +1,6 @@
 const inputText = document.getElementById("input");
 const addBtn = document.querySelector("#addBtn");
 let items = document.getElementById("todoList");
-//let array = [];
 
 addBtn.addEventListener("click", function (event) {
     event.preventDefault();  // Förhindrar siduppdatering
@@ -12,13 +11,28 @@ function läggTill() {
     const läggTill = document.createElement("li");
     läggTill.textContent = inputText.value;
     läggTill.classList.add("todoItem");
-    items.appendChild(läggTill);
+    if (inputText.value !== "") {
+        items.appendChild(läggTill);
+        //här gnereras gjort knappen.
+        
+        const gjortBtn = document.createElement("button");
+        gjortBtn.textContent = "done"
+        läggTill.appendChild(gjortBtn);
+        gjortBtn.classList.add("gjortBtn")
+        // event listener till gjort knappen.
+        gjortBtn.addEventListener("click", function(){
+            console.log("done klicked");
+            läggTill.classList.toggle("done");
+        });
+        //Här genereras ta bort knapp i värje ny element i todo list.
+        const tabortBtn = document.createElement("button");
+        tabortBtn.textContent = "ta bort";
+        läggTill.appendChild(tabortBtn);
+        tabortBtn.classList.add("taborBtn")
+        tabortBtn.addEventListener("click", function () {
+            items.removeChild(läggTill);
+        });
+    }
     // tabort knappen ska vara här nere
-    const tabortBtn = document.createElement("button");
-    tabortBtn.textContent = "ta bort";
-    tabortBtn.addEventListener("click", function () {
-        items.removeChild(läggTill);
-    });
-    läggTill.appendChild(tabortBtn);
     inputText.value = "";
 }
