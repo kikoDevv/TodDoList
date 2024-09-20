6e4de2088f10e384f0d304372eb3961550580ce4
Nasrolla <Nasrolla.hassani@gmail.com>
2024-09-20 17:33:44 +0200

    tabort knappen är klart. behövs styling!

--- BackEnd.js
+++ BackEnd.js
@@ -3,14 +3,22 @@ const addBtn = document.querySelector("#addBtn");
 let items = document.getElementById("todoList");
 //let array = [];
 
-addBtn.addEventListener("click", function(event) {
+addBtn.addEventListener("click", function (event) {
     event.preventDefault();  // Förhindrar siduppdatering
     läggTill();
 });
 
-function läggTill(){
+function läggTill() {
     const läggTill = document.createElement("li");
     läggTill.textContent = inputText.value;
     läggTill.classList.add("todoItem");
     items.appendChild(läggTill);
+    // tabort knappen ska vara här nere
+    const tabortBtn = document.createElement("button");
+    tabortBtn.textContent = "ta bort";
+    tabortBtn.addEventListener("click", function () {
+        items.removeChild(läggTill);
+    });
+    läggTill.appendChild(tabortBtn);
+    inputText.value = "";
 }
